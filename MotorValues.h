872b5b3167e33d928269a661453a8d6c8ce85728
Alex/sprocket <Alexhb61@users.noreply.github.com>
2014-11-30 20:18:45 -0800

    Update MotorValues.h
    I Just realized that I have to create a class for the encoders. FUUUUUCLCLCLCJKEFIOphbfeiaods'kapofajseior;lghjsjlkdf

--- MotorValues.h
+++ MotorValues.h
@@ -8,20 +8,19 @@ struct MotorValues {
   	int pin, analogOffset; 
 // for the absolute encoder connection and the movement/speed digital encoder
   	ROEncoder* SpeedE;// the encoder on the wheel
-  	ROAnalog* RotationE;// the encoder on the top of the motor hub
+  	KCEncoder* RotationE;// the encoder on the top of the motor hub
 
   PIDlong distance;// This is used to calculate velocity
   PIDlong time;// REMEBER TO CONVERT to long as this is an unsigned long.
   PIDdouble velocity; // this is used to calculate wierdness
   PIDdouble assigned; // these values need to be after the inversion
  
-  	bool working;//This is a boolean saying whether or not the 
-  	double slope;// this is the weirdness/ efficiency
-  	double intercept;
+  	bool working;//This is a boolean saying whether or not the motor is moving
+     
+  	//replaces lincomp and a few other things.
+  mx_b wierdness;
 // motor values that would be sent are kept in the same array the whole time.
   	void derive();
-//does the composition of the two functions needed to account for weirdness  
-  	double lincomp(double m,double b, double generated);
   	MotorValues(int num);
   
 };
